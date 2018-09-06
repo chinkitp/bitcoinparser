59806e59f2e90daa2e8c2b4981b98bc078a987d3
Chinkit Patel <chinkit.patel@unisa.edu.au>
2018-09-06 12:49:50 +0930

    Simplified error handling
    refactor names
    
    better readability

--- bitcoinparser/core/src/main/scala/com/chinkit/bitcoinparser/App.scala
+++ bitcoinparser/core/src/main/scala/com/chinkit/bitcoinparser/App.scala
@@ -26,17 +26,26 @@ object App {
         conf.setIfMissing("spark.master", "local[*]")
         conf.setIfMissing("spark.app.name", getClass.getSimpleName)
 
+        val INPUT_DIR = args(0)
+        val OUTPUT_DIR = args(1)
+        val ERROR_DIR = args(2)
+
         val session = SparkSession.builder()
             .config(conf)
             .getOrCreate()
 
-        val files: RDD[(String, PortableDataStream)] = session.sparkContext.binaryFiles(args(0))
+        var blockStoreFileWithEpgmElements = session.sparkContext.binaryFiles(INPUT_DIR)
+                .map(f => (f._1, Try(BitcoinStore.parse(f._2))))
 
-        var parsedFiles: RDD[(String, Seq[String])] = files.map(f => BitcoinBlockParser.FileParse(f))
+        blockStoreFileWithEpgmElements.filter(f => f._2.isSuccess)
+                .map(f => f._2.get)
+                .flatMap(f => f)
+                .coalesce(50)
+                .saveAsTextFile(OUTPUT_DIR)
 
-        parsedFiles.flatMap(f => f._2)
-            .saveAsTextFile(args(2))
-
+        blockStoreFileWithEpgmElements.filter(f => f._2.isFailure)
+                .map(f =>  (f._1,f._2))
+                .saveAsTextFile(ERROR_DIR)
 
         session.close()
 
@@ -58,117 +67,86 @@ object App {
         val props = scala.collection.mutable.Map[String, String]()
     }*/
 
-    object BitcoinBlockParser {
-        var params = MainNetParams.get;
+    object BitcoinStore {
+        val params = MainNetParams.get;
+        val c = Context.getOrCreate(params)
+
         var mapper = new ObjectMapper()
         mapper.registerModule(DefaultScalaModule)
 
-        def FileParse(f: (String, PortableDataStream)): (String, Seq[String]) = {
-
-            val blocks: Seq[Try[Block]] = BitcoinBlockParser.Parse(f._1, f._2)
-
-            val successBlocks: Seq[String] = blocks.filter(b => b.isSuccess)
-                .map(b => Convert(b.get))
-                .flatten(b => b)
-
-
-            val failedBlocks: Seq[String] = blocks.filter(b => b.isFailure)
-                .map(b => {
-                    var exp = b.asInstanceOf[Failure[BadBlockException]].exception
-                            .asInstanceOf[BadBlockException]
-                    Seq[String](exp.filename,exp.message)
-                })
-                .flatten(b => b)
-
-            (f._1,Seq concat(successBlocks, failedBlocks,Array("")))
-
+        def parse(blockStoreFile: PortableDataStream) : Seq[String] = {
+            splitIntoBlocks(blockStoreFile.toArray())
+                .map(blockBytes => getBlock(blockBytes))
+                .flatMap(block => getEpgmElements(block))
         }
 
-        def Convert(f: Block): Seq[String] = {
+        private def getEpgmElements(f: Block): Seq[String] = {
 
-            try{
-                val block = new Node(
-                    f.getHashAsString,
-                    "block",
+            val block = new Node(
+                f.getHashAsString,
+                "block",
+                Array.empty,
+                getBlockData(f)
+            )
+
+            val transactions = f.getTransactions.toArray(new Array[Transaction](f.getTransactions.size()))
+                .map(t => new Node(
+                    t.getHashAsString,
+                    label = "transaction",
                     Array.empty,
-                    Map(
-                        "PreviousBlockHash" -> f.getPrevBlockHash.toString,
-                        "Nonce" -> f.getNonce.toString,
-                        "Difficulty" -> f.getDifficultyTarget.toString,
-                        "Version" -> f.getVersion.toString,
-                        "MerkleRoot" -> f.getMerkleRoot.toString,
-                        "Time" -> f.getTime.toString
-                    )
-                )
+                    getTransactionData(t)
+                ))
 
-                var transactions = new Array[App.Node](0)
-                if(f.getTransactions != null && f.getTransactions.size() > 0){
-                    transactions = f.getTransactions.toArray(new Array[Transaction](f.getTransactions.size()))
-                        .map(t => new Node(
-                            t.getHashAsString,
-                            label = "transaction",
-                            Array.empty,
-                            Map(
-                                "isCoinBase" -> t.isCoinBase.toString,
-                                "isTimeLocked" -> t.isTimeLocked.toString,
-                                "memo" -> t.getMemo,
-                                "version" -> t.getVersion.toString
-                            )
-                        ))
-                }
-
-                return (Seq concat(transactions, Array(block)))
-                    .map(b => mapper.writeValueAsString(b))
-
-            }
-            catch{
-                case e: Exception =>
-                    println(e.getMessage)
-            }
-
-            Array("")
+            return (Seq concat(transactions, Array(block)))
+                .map(b => mapper.writeValueAsString(b))
         }
 
-
-        def Parse(f: (String, PortableDataStream)): Seq[Try[Block]] = {
-
-            //This line is required to init the context.
-            val c = Context.getOrCreate(params)
-
-            var blocks = Seq.newBuilder[Try[Block]]
-
-            try
-            {
-                val fileBytes = f._2.toArray()
-                //println("Size of the available block : " + fileBytes.length)
-
-                //stream.readFully(fileBytes)
-                var byteCursor = 0
-                var blockCount = 0
-
-                while (IsMagic(fileBytes, byteCursor)) {
-
-                    blockCount = blockCount + 1;
-
-                    var blockSizeBytes = new Array[Byte](4)
-                    System.arraycopy(fileBytes,byteCursor + 4,blockSizeBytes,0,4)
-                    val blockSize = Utils.readUint32BE(Utils.reverseBytes(blockSizeBytes), 0)
-                    var blockBytes = new Array[Byte](blockSize.toInt)
-                    System.arraycopy(fileBytes,byteCursor + 8,blockBytes,0, blockSize.toInt);
-                    var block = params.getDefaultSerializer.makeBlock(blockBytes);
-                    byteCursor = byteCursor + 8 + blockSize.toInt;
-                    blocks += Success(block)
-                }
-            }
-            catch {
-                case e: Exception =>
-                    blocks += Failure(new BadBlockException(f._1,e.getMessage))
-            }
-
-            return  blocks.result()
+        private def getTransactionData(t: Transaction) = {
+            Map(
+                "isCoinBase" -> t.isCoinBase.toString,
+                "isTimeLocked" -> t.isTimeLocked.toString,
+                "memo" -> t.getMemo,
+                "version" -> t.getVersion.toString
+            )
         }
 
-        private def IsMagic(bytes: Array[Byte], startSeq: Int): Boolean= {
+        private def getBlockData(f: Block) = {
+            Map(
+                "PreviousBlockHash" -> f.getPrevBlockHash.toString,
+                "Nonce" -> f.getNonce.toString,
+                "Difficulty" -> f.getDifficultyTarget.toString,
+                "Version" -> f.getVersion.toString,
+                "MerkleRoot" -> f.getMerkleRoot.toString,
+                "Time" -> f.getTime.toString
+            )
+        }
+
+        private def splitIntoBlocks(t: Array[Byte]) : Seq[Array[Byte]] = {
+            var blocksAsBytes = Seq.newBuilder[Array[Byte]]
+            var byteCursor = 0
+            var blockCount = 0
+
+            while (areNextBytesMagic(t, byteCursor)) {
+
+                blockCount = blockCount + 1;
+
+                var blockSizeBytes = new Array[Byte](4)
+                System.arraycopy(t, byteCursor + 4, blockSizeBytes,0,4)
+                val blockSize = Utils.readUint32BE(Utils.reverseBytes(blockSizeBytes), 0)
+                var blockBytes = new Array[Byte](blockSize.toInt)
+                System.arraycopy(t,byteCursor + 8,blockBytes,0, blockSize.toInt);
+                byteCursor = byteCursor + 8 + blockSize.toInt;
+                blocksAsBytes += blockBytes
+            }
+
+            blocksAsBytes.result()
+        }
+
+        private def getBlock(b: Array[Byte]) : Block = {
+            params.getDefaultSerializer.makeBlock(b);
+        }
+
+        private def areNextBytesMagic(bytes: Array[Byte], startSeq: Int): Boolean= {
             if ((bytes.size != startSeq)
                 && (bytes(startSeq) & 0xFF) == 0xF9
                 && (bytes(startSeq + 1) & 0xFF) == 0xbe
@@ -183,4 +161,3 @@ object App {
 
 }
 
-case class BadBlockException(filename: String, message: String) extends Exception(message)
